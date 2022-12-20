f33d56e5f53c12bbf6356e4192ae588a07e499e5
Elwyn Van der Borght <elwyn.vdb@gmail.com>
2022-12-20 19:03:38 +0100

    Query for Detail page

--- src/pages/plcs/{wpPlc.slug}.tsx
+++ src/pages/plcs/{wpPlc.slug}.tsx
@@ -1,7 +1,41 @@
+import { graphql } from "gatsby";
 import React from "react";
 
-const PlcPage = () => {
+const PlcPage = ({ data }: { data: any }) => {
     return <></>
 }
+
+export const plcQuery = graphql`
+query plcQuery($slug: String) {
+  wpPlc(slug: { eq: $slug }) {
+    plcFields {
+      width
+      usageEnvironment
+      series
+      serialNumber
+      programmingPossibilties
+      portType
+      operatingVoltage
+      operatingCurrent
+      outputs
+      length
+      inputs
+      height
+      mainPicture {
+        gatsbyImage(width: 500, placeholder: BLURRED)
+      }
+      promoImage1 {
+        gatsbyImage(width: 500, placeholder: BLURRED)
+      }
+      promoImage2 {
+        gatsbyImage(width: 500, placeholder: BLURRED)
+      }
+      promoImage3 {
+        gatsbyImage(width: 500, placeholder: BLURRED)
+      }
+    }
+  }
+}
+`;
 
 export default PlcPage;
