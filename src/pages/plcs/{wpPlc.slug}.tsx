35861601bb9664385a7e4b89349fe384e21d8e8c
Elwyn Van der Borght <elwyn.vdb@gmail.com>
2023-01-01 12:08:14 +0100

    Displat of taxonomies

--- src/pages/plcs/{wpPlc.slug}.tsx
+++ src/pages/plcs/{wpPlc.slug}.tsx
@@ -5,40 +5,46 @@ import PlcSpecifications from "../../components/PlcSpecifications";
 import NamedSection from "../../components/NamedSection";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import TagDisplay from "../../components/TagDisplay";
+import { NameNode } from "../../interfaces/NameNode";
 
-const PlcPage = ({ data: { wpPlc: { plcFields } } }: { data: Data }) => {
-  const goBackText = "< Go Back"
+const PlcPage = ({ data: { wpPlc: { plcFields, types, methods, outputs } } }: { data: Data }) => {
+  const goBackText = "< Go Back";
+
+  const mapNames = (entry:NameNode) => entry.nodes.map(n => n.name);  
+
+  const combinedTags = [...mapNames(types), ...mapNames(methods), ...mapNames(outputs)];
+
   return <div className="plc-details wrapper">
     <Link className="plc-details-back" to="/plcs/">{goBackText}</Link>
     <section>
       <h1 className="plc-details__title">{plcFields.serialNumber}</h1>
-      <TagDisplay tags={["Hi", "hola"]}/>
+      <TagDisplay tags={combinedTags} />
     </section>
     <section className="wrapper">
-    <GatsbyImage className="plc-details__image" image={getImage(plcFields.mainPicture.localFile.childImageSharp.gatsbyImageData)!} alt={plcFields.mainPicture.altText}/>
-    <NamedSection title="Properties">
-      <PlcSpecifications specifications={[
-        ["Series", [plcFields.series]],
-        ["Serial Number", [plcFields.serialNumber]],
-        ["Input Count", [plcFields.inputs]],
-        ["Output Count", [plcFields.outputs]],
-        ["Output Type", [plcFields.portType]],
-        ["Operating Voltage", [plcFields.operatingVoltage, "V"]],
-        ["Operating Current", [plcFields.operatingCurrent, "mA"]],
-        ["Width", [plcFields.width]],
-        ["Height", [plcFields.height]],
-        ["Length", [plcFields.length]],
-        ["Programming Possibilities", plcFields.programmingPossibilties],
-        ["Environment", [plcFields.usageEnvironment]]
-      ]} />
-    </NamedSection>
+      <GatsbyImage className="plc-details__image" image={getImage(plcFields.mainPicture.localFile.childImageSharp.gatsbyImageData)!} alt={plcFields.mainPicture.altText} />
+      <NamedSection title="Properties">
+        <PlcSpecifications specifications={[
+          ["Series", [plcFields.series]],
+          ["Serial Number", [plcFields.serialNumber]],
+          ["Input Count", [plcFields.inputs]],
+          ["Output Count", [plcFields.outputs]],
+          ["Output Type", [plcFields.portType]],
+          ["Operating Voltage", [plcFields.operatingVoltage, "V"]],
+          ["Operating Current", [plcFields.operatingCurrent, "mA"]],
+          ["Width", [plcFields.width]],
+          ["Height", [plcFields.height]],
+          ["Length", [plcFields.length]],
+          ["Programming Possibilities", plcFields.programmingPossibilties],
+          ["Environment", [plcFields.usageEnvironment]]
+        ]} />
+      </NamedSection>
     </section>
   </div>
 }
 
 export const plcQuery = graphql`
 query plcQuery($slug: String) {
-  wpPlc(slug: { eq: $slug }) {
+  wpPlc(slug: {eq: $slug}) {
     plcFields {
       width
       usageEnvironment
@@ -58,15 +64,15 @@ query plcQuery($slug: String) {
           childImageSharp {
             gatsbyImageData(placeholder: BLURRED)
           }
+        }
       }
-    }
       promoImage1 {
         altText
         localFile {
           childImageSharp {
             gatsbyImageData(placeholder: BLURRED)
           }
-      }
+        }
       }
       promoImage2 {
         altText
@@ -74,7 +80,7 @@ query plcQuery($slug: String) {
           childImageSharp {
             gatsbyImageData(placeholder: BLURRED)
           }
-      }
+        }
       }
       promoImage3 {
         altText
@@ -85,12 +91,30 @@ query plcQuery($slug: String) {
         }
       }
     }
+    types {
+      nodes {
+        name
+      }
+    }
+    outputs {
+      nodes {
+        name
+      }
+    }
+    methods {
+      nodes {
+        name
+      }
+    }
   }
 }
 `;
 
 export interface WpPlc {
   plcFields: PlcFields;
+  types: NameNode;
+  outputs: NameNode;
+  methods: NameNode;
 }
 
 export interface Data {
