a02054e33f63fd0eca2db29b48505711b68e0557
Elwyn Van der Borght <elwyn.vdb@gmail.com>
2023-01-03 12:06:25 +0100

    Giving products page a bit more randomness

--- src/pages/plcs/index.tsx
+++ src/pages/plcs/index.tsx
@@ -5,14 +5,12 @@ import { Edges } from "../../interfaces/Edges";
 import { MainData } from "../../interfaces/MainData";
 import { PlcFields } from "../../interfaces/plc/PlcFields";
 import { ProductsFields } from "../../interfaces/products/ProductsFields";
-import parse from "html-react-parser";
-import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import PlcCardsDisplay from "../../components/PlcCardsDisplay";
 import NamedSection from "../../components/NamedSection";
 import Hero from "../../components/Hero";
 
 const Plcs: FC<MainData<PLCSData>> = ({ data: { wpPage: { products_fields }, allWpPlc: { edges } } }) => {
-  const plcFields = edges.map(e => e.node.plcFields);
+  const plcFields = edges.map(e => e.node.plcFields).sort((a, b) => Math.random() < 0.5 ? 1 : -1);
 
   return <div className="wrapper plcs">
     <Hero title={products_fields.title} description={products_fields.description} image={products_fields.picture} />
