9fe74110c44bc57cd04ac1519e7a1f29e92bffef
Elwyn Van der Borght <elwyn.vdb@gmail.com>
2023-01-03 12:05:03 +0100

    Feature for custom classNames in NamedSection

--- src/components/NamedSection.tsx
+++ src/components/NamedSection.tsx
@@ -2,14 +2,17 @@ import React, { FC } from "react";
 
 interface NamedSection {
     title: string;
+    className?: string;
     children: React.ReactNode;
     center?: boolean;
 }
 
-const NamedSection: FC<NamedSection> = ({title, children, center = true}) => {
+const NamedSection: FC<NamedSection> = ({ title, children, center = true, className = "" }) => {
     return <section className={`named__section${center ? " named__section__center" : ""}`}>
         <h1>{title}</h1>
-        {children}
+        <div className={className}>
+            {children}
+        </div>
     </section>
 }
 
